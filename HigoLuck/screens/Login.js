b1a7a91a0f3f1de2fb1bcf5d1cd642811c618253
Ronny Alvarez <ronny@grupomundial.cr>
2021-06-16 22:16:46 -0600

    Include fetch request

--- HigoLuck/screens/Login.js
+++ HigoLuck/screens/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, Image, TextInput } from 'react-native';
+import { StyleSheet, View, Text, Button, Image, TextInput, Alert } from 'react-native';
 
 const styles = StyleSheet.create({
     background:{
@@ -12,7 +12,58 @@ const styles = StyleSheet.create({
       },
 });
 
+const showAlert = () =>
+  Alert.alert(
+    "Error",
+    "Usuario Invalido",
+    [
+      {
+        text: "Cancel",
+        onPress: () => Alert.alert("Cancel Pressed"),
+        style: "cancel",
+      },
+    ],
+    {
+      cancelable: true,
+      onDismiss: () =>
+        Alert.alert(
+          "This alert was dismissed by tapping outside of the alert dialog."
+        ),
+    }
+  );
+
 const Login = ({ navigation }) => {
+
+    const isUserValid = async () => {
+        let response = await fetch(
+          'http://161.97.71.194:5000/user/login',
+          {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                userName:"donny_2",
+                password:"123456789"
+            })
+          }
+        );
+        let json = await response.json();
+
+        let entrar = false;
+
+        console.log(json);
+
+        if(json.userId != undefined){
+            navigation.navigate('Home');
+        }else{
+            showAlert();
+        }
+
+      }
+    
+
     return (
         <View style={styles.background}>
             <View>
@@ -26,7 +77,7 @@ const Login = ({ navigation }) => {
       />
             </View>
             <View>
-            <Button title="Ingresar" onPress={()=> navigation.navigate('Home')}/>
+            <Button title="Ingresar" onPress={() => isUserValid()}/>
             </View>
         </View>
 
