31b5f0c605752199e535d2878979e839bb1783ec
Ronny Alvarez <ronny@grupomundial.cr>
2021-06-16 23:11:17 -0600

    Settings

--- HigoLuck/screens/Home.js
+++ HigoLuck/screens/Home.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: '#FFFFFF'
+    },
+    view: {
+        padding: 5,
+        margin: 5,
+    },
+});
 
 const Home = ({ navigation }) => {
     return (
         <View>
-            <Button title='Vender' onPress={()=> navigation.navigate('MainScreen')} />
-            <Button title='Reclamar' onPress={()=> navigation.navigate('Reward')} />
-            <Button title='Configuraciones' onPress={()=> navigation.navigate('Settings')} />
+            <View style={styles.view}>
+                <Button title='Vender' onPress={()=> navigation.navigate('MainScreen')} />
+             </View>
+            <View style={styles.view}>
+                <Button title='Reclamar' onPress={()=> navigation.navigate('Reward')} />
+            </View>
+            <View style={styles.view}>
+                <Button title='Configuraciones' onPress={()=> navigation.navigate('Settings')} />
+            </View>
         </View>
     )
 }
--- HigoLuck/screens/Settings.js
+++ HigoLuck/screens/Settings.js
@@ -1,12 +1,58 @@
-import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: '#FFFFFF'
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+    },
+});
 
 const Settings = (props) => {
-    return (
-        <View>
-            <Text>Configuraciones</Text>
-            <ScrollView style={{ flex: 1 }} />
+    const [user, setUser] = useState('');
+    const [pass, setPass] = useState('');
 
+    return (
+        <View style={styles.background}>
+            <View>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setUser}
+                    value={user}
+                    placeholder = {'Nombre'}
+                />
+            </View>
+            <View>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setUser}
+                    value={user}
+                    placeholder = {'Correo'}
+                />
+            </View>
+            <View>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setUser}
+                    value={user}
+                    placeholder = {'Usuario'}
+                />
+            </View>
+            <View>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setPass}
+                    value={pass}
+                    placeholder = {'Contraseña'}
+                />
+            </View>
+            <View>
+                <Button title="Ingresar" onPress={() => isUserValid()} />
+            </View>
         </View>
     )
 }
