41fd25e817329d2f6ccbe6ec7fd5f8731c815d81
Ronny Alvarez <ronny@grupomundial.cr>
2021-06-19 17:57:48 -0600

    new style add number

--- HigoLuck/components/AddNumber.js
+++ HigoLuck/components/AddNumber.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Image, TextInput, Picker, Button, Alert } from 'react-native';
+import { Text, View, StyleSheet, Image, TextInput, Button, Alert,TouchableOpacity } from 'react-native';
 import { addNumber } from '../store/Actions';
 import { useDispatch } from 'react-redux';
 import { RadioButton } from 'react-native-paper';
+import { Checkbox, List, Switch } from 'react-native-paper';
 
 
 const AddNumber = (props) => {
@@ -11,6 +12,8 @@ const AddNumber = (props) => {
   const [amount, setAmount] = useState();
   const [dayNaight, setDayNaight] = useState('Day');
   const [value, setValue] = React.useState('first');
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
 
   let numberElement = {
     number: number,
@@ -20,49 +23,51 @@ const AddNumber = (props) => {
 
   return (
     <View>
-      <TextInput
-        style={styles.input}
-        onChangeText={setNumber}
-        value={number}
-        placeholder="Número"
-        keyboardType="numeric"
-        maxLength={2}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={setAmount}
-        value={amount}
-        placeholder="Monto"
-        keyboardType="numeric"
-        maxLength={5}
-      />
-
-<View style={styles.container}>
-
-      <RadioButton.Group onValueChange={newValue => setValue(newValue)} value={value}>
-        <View style={styles.container}>
-        <View style={styles.box1}>
-          <Text>First</Text>
-          <RadioButton value="first" />
-        </View>
-        <View style={styles.box2}>
-          <Text>Second</Text>
-          <RadioButton value="second" />
-        </View>
-        </View>
-      </RadioButton.Group>
-      <RadioButton.Group onValueChange={newValue => setValue(newValue)} value={value}>
-        <View style={styles.container}>
-        <View style={styles.box1}>
-          <Text>First</Text>
-          <RadioButton value="first" />
-        </View>
-        <View style={styles.box2}>
-          <Text>Second</Text>
-          <RadioButton value="second" />
-        </View>
-        </View>
-      </RadioButton.Group>
+      <View>
+        <TextInput
+          style={styles.input}
+          onChangeText={setNumber}
+          value={number}
+          placeholder="Número"
+          keyboardType="numeric"
+          maxLength={2}
+        />
+      </View>
+      <View>
+        <TextInput
+          style={styles.input}
+          onChangeText={setAmount}
+          value={amount}
+          placeholder="Monto"
+          keyboardType="numeric"
+          maxLength={5}
+        />
+      </View>
+      <View style={styles.container}>
+        <RadioButton.Group onValueChange={newValue => setValue(newValue)} value={value}>
+          <View style={styles.container}>
+            <View style={styles.box1}>
+              <Text>First</Text>
+              <RadioButton value="first" />
+            </View>
+            <View style={styles.box2}>
+              <Text>Second</Text>
+              <RadioButton value="second" />
+            </View>
+          </View>
+        </RadioButton.Group>
+        <RadioButton.Group onValueChange={newValue => setValue(newValue)} value={value}>
+          <View style={styles.container}>
+            <View style={styles.box1}>
+              <Text>First</Text>
+              <RadioButton value="first" />
+            </View>
+            <View style={styles.box2}>
+              <Text>Second</Text>
+              <RadioButton value="second" />
+            </View>
+          </View>
+        </RadioButton.Group>
 
       </View>
 
@@ -77,8 +82,15 @@ const AddNumber = (props) => {
 
       {/* <Button title='Add' onPress={() => dispatch(addNumber(numberElement))} /> */}
 
-
-      <Button title='Add' onPress={() => props.InsertNumber(numberElement)} />
+      <View style={styles.containerB}>
+      <View>
+        <Text>Reventado</Text>
+        <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={() => props.InsertNumber(numberElement)}>
+        <Text style={styles.buttonText}>Agregar</Text>
+      </TouchableOpacity>
+    </View>
     </View>
   )
 }
@@ -91,15 +103,30 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
   },
+  containerB: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     margin: 12,
-    flex: 1, 
-    flexDirection: 'row',   
+    flex: 1,
+    flexDirection: 'row',
   },
   box1: {
     width: 80,
   },
   box2: {
     width: 80,
+  },
+  button: {
+    margin: 10,
+    paddingHorizontal: 50,
+    paddingVertical: 10,
+    borderRadius: 10,
+    backgroundColor: 'steelblue',
+  },
+  buttonText: {
+    color: 'white',
   },
 });
